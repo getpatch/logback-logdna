db5c75795e6c74d06b8f54cca59c2d183c15a076
archimed <archimed.shaman@gmail.com>
2021-08-06 16:09:06 +0300

    Make hostname configurable

--- src/main/java/net/_95point2/utils/LogDNAAppender.java
+++ src/main/java/net/_95point2/utils/LogDNAAppender.java
@@ -25,44 +25,38 @@ public class LogDNAAppender extends UnsynchronizedAppenderBase<ILoggingEvent>
 {
 	/** relying on the guard in the parent implementation to prevent recursive calls, hoping there is another ERROR destination */
 	Logger emergencyLog = LoggerFactory.getLogger(LogDNAAppender.class);
-	
-	private String hostname;
+
 	private Webb http;
-	
+
 	/*
 	 * configurables ( plus ingestKey )
 	 */
+	private String hostname = getHostName();
 	private String appName;
 	private boolean includeStacktrace = true;
 	private boolean sendMDC = true;
-	
+
 	public LogDNAAppender() {
-		try {
-			this.hostname = InetAddress.getLocalHost().getHostName();
-		} catch (UnknownHostException e) {
-			this.hostname = "localhost";
-		}
-		
 		Webb webb = Webb.create();
 		webb.setBaseUri("https://logs.logdna.com/logs/ingest");
 		webb.setDefaultHeader(Webb.HDR_USER_AGENT, "LogDNA Appender (95point2)");
 		this.http = webb;
 	}
-	
+
 	@Override
-	protected void append(ILoggingEvent ev) 
+	protected void append(ILoggingEvent ev)
 	{
 		/* not interested in consuming our own filth */
 		if(ev.getLoggerName().equals(LogDNAAppender.class.getName())){
 			return;
 		}
-		
+
 		StringBuilder sb = new StringBuilder()
 				.append("[").append(ev.getThreadName()).append("] ")
 				.append(ev.getLoggerName())
 				.append(" -- ")
 				.append(ev.getFormattedMessage());
-		
+
 		if(ev.getThrowableProxy() != null && this.includeStacktrace){
 			IThrowableProxy tp = ev.getThrowableProxy();
 			sb.append("\n\n").append(tp.getClassName()).append(": ").append(tp.getMessage());
@@ -70,41 +64,41 @@ public class LogDNAAppender extends UnsynchronizedAppenderBase<ILoggingEvent>
 				sb.append("\n\t").append(ste.getSTEAsString());
 			}
 		}
-		
+
 		try
 		{
 			JSONObject payload = new JSONObject();
 			JSONArray lines = new JSONArray();
 			payload.put("lines", lines );
-			
+
 			JSONObject line = new JSONObject();
 			line.put("timestamp", ev.getTimeStamp());
 			line.put("level", ev.getLevel().toString());
 			line.put("app", this.appName);
 			line.put("line", sb.toString());
-			
+
 			JSONObject meta = new JSONObject();
 			meta.put("logger", ev.getLoggerName());
 			line.put("meta", meta);
-			
+
 			if(this.sendMDC && !ev.getMDCPropertyMap().isEmpty()){
 				for(Entry<String,String> entry : ev.getMDCPropertyMap().entrySet()){
 					meta.put(entry.getKey(), entry.getValue());
 				}
 			}
-			
-			
+
+
 			lines.put(line);
-			
+
 			HashMap<String,Object> params = new HashMap<>();
 			params.put("hostname", this.hostname);
 			params.put("now", System.currentTimeMillis());
-			
+
 			Response<JSONObject> response = http.post("?hostname=" + encode(this.hostname) + "&now=" + encode(String.valueOf(System.currentTimeMillis())) )
 					.body(payload)
 					.retry(3, true)
 					.asJsonObject();
-			
+
 			if(!response.isSuccess()){
 				String msg = "Error posting to LogDNA ";
 				msg += response.getStatusCode() + " ";
@@ -117,9 +111,9 @@ public class LogDNAAppender extends UnsynchronizedAppenderBase<ILoggingEvent>
 			emergencyLog.error(e.getMessage(), e);
 		}
 	}
-	
-	
-	
+
+
+
 	public void setAppName(String appName) {
 		this.appName = appName;
 	}
@@ -127,7 +121,7 @@ public class LogDNAAppender extends UnsynchronizedAppenderBase<ILoggingEvent>
 	public void setIngestKey(String ingestKey) {
 		this.http.setDefaultHeader("apikey", ingestKey);
 	}
-	
+
 	public void setSendMDC(boolean sendMDC) {
 		this.sendMDC = sendMDC;
 	}
@@ -136,12 +130,20 @@ public class LogDNAAppender extends UnsynchronizedAppenderBase<ILoggingEvent>
 		this.includeStacktrace = includeStacktrace;
 	}
 
+	private static String getHostName() {
+		try {
+			return InetAddress.getLocalHost().getHostName();
+		} catch (UnknownHostException e) {
+			return "localhost";
+		}
+	}
+
 	private static String encode(String str) {
-        try 
+        try
         {
             return URLEncoder.encode(str, "UTF-8");
-        } 
-        catch (UnsupportedEncodingException e) 
+        }
+        catch (UnsupportedEncodingException e)
         {
             return str;
         }
